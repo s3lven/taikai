802f96899e0dc6375cb24f5eacf0218ef723ecd8
Eriz <erizjsub2@gmail.com>
2025-03-07 04:47:43 -0800

    fix: added back results dialog

--- frontend/src/pages/bracket-page.tsx
+++ frontend/src/pages/bracket-page.tsx
@@ -1,12 +1,12 @@
 import BracketPanel from "../features/bracket/bracket-panel/bracket-panel";
 import BracketView from "@/features/bracket/bracket-view/bracket-view";
+import BracketDialogManager from "@/features/bracket/components/bracket-dialog-manager";
 import useBracketQuery from "@/features/bracket/hooks/useBracketQuery";
 import { useParams } from "react-router-dom";
 
 const BracketPage = () => {
   // Extract the bracket id from the URL and check if it exists
   const params = useParams();
-
   if (params.bracketId === undefined) {
     console.error("Bracket ID not found in URL");
   }
@@ -22,6 +22,7 @@ const BracketPage = () => {
         <BracketPanel />
         <BracketView />
       </div>
+      <BracketDialogManager />
     </div>
   );
 };
