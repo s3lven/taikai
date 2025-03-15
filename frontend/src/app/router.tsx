7eafc14dc96da0ede8acdd7aec6266d5539b0246
Eriz <erizjsub2@gmail.com>
2025-03-14 21:24:09 -0700

    feat: added protected route and navbar

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,20 +1,34 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Outlet, Route, Routes } from "react-router-dom"
 import MainLayout from "../components/layout/main-layout"
 import HomePage from "../pages/home-page"
 import DashboardPage from "@/pages/dashboard-page"
 import BracketPage from "@/pages/bracket-page"
 import LoginPage from "@/pages/login-page"
 import ProfilePage from "@/pages/profile-page"
+import useAuth from "@/hooks/useAuth"
+
+const ProtectedRoute = () => {
+  const { session } = useAuth()
+
+  if (!session) {
+    return <Navigate to={"login"} replace />
+  }
+
+  return <Outlet />
+}
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/bracket/:bracketId" element={<BracketPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/bracket/:bracketId" element={<BracketPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Route>
       </Route>
       <Route path="*" element={<Navigate to={"/"} replace />} />
     </Routes>
