015918dc1de200ee7d963bfd1a0e4aaeb834618f
Eriz <erizjsub2@gmail.com>
2025-03-14 18:58:05 -0700

    refactor: replaced app router structure

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,21 +1,18 @@
-import { useRoutes } from "react-router-dom";
-import MainLayout from "../components/layout/main-layout";
-import HomePage from "../pages/home-page";
-import DashboardPage from "@/pages/dashboard-page";
-import BracketPage from "@/pages/bracket-page";
+import { Navigate, Route, Routes } from "react-router-dom"
+import MainLayout from "../components/layout/main-layout"
+import HomePage from "../pages/home-page"
+import DashboardPage from "@/pages/dashboard-page"
+import BracketPage from "@/pages/bracket-page"
 
 export const AppRoutes = () => {
-  const routes = [
-    {
-      element: <MainLayout />,
-      children: [
-        { path: "/", element: <HomePage /> },
-        { path: "/dashboard", element: <DashboardPage /> },
-        { path: "/bracket/:bracketId", element: <BracketPage /> },
-      ],
-    },
-  ];
-
-  const element = useRoutes([...routes]);
-  return <>{element}</>;
-};
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/bracket/:bracketId" element={<BracketPage />} />
+      </Route>
+      <Route path="*" element={<Navigate to={"/"} replace />} />
+    </Routes>
+  )
+}
