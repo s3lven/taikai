5d5f2a0aa064c2496521cd0e1d171ae846c59e30
Eriz <erizjsub2@gmail.com>
2025-03-16 03:21:53 -0700

    chore: adjusted db config for build

--- backend/db/knexfile.ts
+++ backend/db/knexfile.ts
@@ -1,7 +1,7 @@
-import type { Knex } from "knex";
-import dotenv from "dotenv";
+import type { Knex } from "knex"
+import dotenv from "dotenv"
 
-dotenv.config({ path: "../.env" });
+dotenv.config({ path: "../.env" })
 
 const config: { [key: string]: Knex.Config } = {
   development: {
@@ -14,6 +14,16 @@ const config: { [key: string]: Knex.Config } = {
       extension: "ts",
     },
   },
-};
+  production: {
+    client: "pg",
+    connection: {
+      connectionString: process.env.PG_CONNECTION_STRING || "",
+    },
+    migrations: {
+      directory: "./migrations",
+      extension: "ts",
+    },
+  },
+}
 
-export default config;
+export default config
