e0aa492d8d2dded1c31aced5e351abc309822347
Eriz <erizjsub2@gmail.com>
2025-03-18 14:47:54 -0700

    refactor: added mobile/tablet layouts for home page

--- frontend/src/pages/home-page.tsx
+++ frontend/src/pages/home-page.tsx
@@ -1,38 +1,47 @@
-import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button"
+import { Link } from "react-router-dom"
 
 const HomePage = () => {
   return (
-    <div className="w-screen flex-1 flex justify-center items-center py-[120px] gap-16 bg-figma_dark font-poppins">
-      <div className="flex flex-col justify-center items-center gap-6">
-        <div className="flex flex-col justify-center items-center gap-4">
-          <div className="flex flex-col gap-2">
-            <p className="text-headline text-center text-white">
+    <div className="w-screen flex-1 flex flex-col items-center justify-center bg-figma_dark">
+      <div className="w-full container mx-auto px-4 space-y-6">
+        <div className="space-y-4">
+          <div className="space-y-2">
+            <p className="lg:text-headline text-center text-base tracking-[0.5px] text-white">
               Never miss your next match
             </p>
-            <p className="text-header text-center text-white ">
+            <p className="lg:text-header text-center font-bold text-[32px] tracking-[0.5px] leading-10 text-white ">
               Welcome to Taikai
             </p>
           </div>
-          <p className="text-xl leading-8 text-center text-white text-balance w-1/2">
+          <p className="lg:text-xl lg:leading-8 text-center text-base tracking-[0.5px] text-white text-balance">
             Taikai provides intuitive bracket creation, real-time match updates,
             and easy navigation to ensure your taikai runs smoothly.
           </p>
         </div>
-        <div className="flex gap-6">
-          <Link to={"/dashboard"}>
+        <div className="md:flex gap-4 items-center justify-center">
+          <Link to={"/dashboard"} className="w-full lg:w-fit">
             <Button
-              className="bg-figma_secondary hover:bg-figma_secondary/80 text-white hover:text-white font-poppins"
+              className="bg-figma_secondary hover:bg-figma_secondary/80 text-white hover:text-white font-poppins w-full"
               variant="outline"
               size="lg"
             >
               Get Started
             </Button>
           </Link>
+          {/* <Link to={"/dashboard"} className="w-full lg:w-fit">
+            <Button
+              className="mt-4 md:mt-0 bg-background hover:bg-background/90 text-figma_secondary hover:text-figma_secondary font-poppins w-full"
+              variant="outline"
+              size="lg"
+            >
+              Explore
+            </Button>
+          </Link> */}
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
