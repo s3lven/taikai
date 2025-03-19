04b48d74980e7e9e3fe680f35da525f5df540552
Eriz <erizjsub2@gmail.com>
2025-03-19 14:40:33 -0700

    refactor: added title visibility props to panel components

--- frontend/src/features/bracket/bracket-panel/components/participants-panel/participants-panel.tsx
+++ frontend/src/features/bracket/bracket-panel/components/participants-panel/participants-panel.tsx
@@ -1,49 +1,59 @@
-import { Shuffle } from "lucide-react";
-import EditorButton from "../../../components/editor-button";
-import ParticipantsList from "./participants-list";
-import { useParticipantStore } from "@/stores/participant-store";
-import { useShallow } from "zustand/react/shallow";
-import { useBracketStore } from "@/stores/bracket-store";
+import { Shuffle } from "lucide-react"
+import EditorButton from "../../../components/editor-button"
+import ParticipantsList from "./participants-list"
+import { useParticipantStore } from "@/stores/participant-store"
+import { useShallow } from "zustand/react/shallow"
+import { useBracketStore } from "@/stores/bracket-store"
 
-const ParticipantsPanel = () => {
-	const { addParticipant, shuffleParticipants } = useParticipantStore(
-		useShallow((state) => ({
-			addParticipant: state.addParticipant,
-			shuffleParticipants: state.shuffleParticipants,
-		}))
-	);
+interface ParticipantPanelProps {
+  isTitleVisible?: boolean
+}
 
-	const [status] = useBracketStore(
-		useShallow((state) => [state.bracket.status])
-	);
+const ParticipantsPanel = ({
+  isTitleVisible = true,
+}: ParticipantPanelProps) => {
+  const { addParticipant, shuffleParticipants } = useParticipantStore(
+    useShallow((state) => ({
+      addParticipant: state.addParticipant,
+      shuffleParticipants: state.shuffleParticipants,
+    }))
+  )
 
-	return (
-		<div className="w-full h-max flex flex-col gap-2.5 p-4 pb-80 font-poppins">
-			<p className="text-figma_grey text-label uppercase w-fit">participants</p>
-			<div className="w-full flex flex-col gap-8 px-2 py-4 bg-figma_shade2_30 shadow rounded-sm ">
-				<ParticipantsList />
+  const [status] = useBracketStore(
+    useShallow((state) => [state.bracket.status])
+  )
 
-				{status === "Editing" ? (
-					<div className="w-full flex justify-between">
-						<EditorButton
-							text={<Shuffle size={"24px"} />}
-							onClickHandler={shuffleParticipants}
-						/>
-						<EditorButton
-							text="Add Participant"
-							onClickHandler={addParticipant}
-						/>
-					</div>
-				) : null}
-			</div>
-			{status !== "Editing" ? (
-				<p className="text-center text-grey text-desc">
-					Note: You cannot edit the participants after the bracket has started.
-					Reset the bracket to make changes.
-				</p>
-			) : null}
-		</div>
-	);
-};
+  return (
+    <div className="w-full h-max flex flex-col gap-2.5 px-0 py-4 md:px-4 font-poppins">
+      {isTitleVisible ? (
+        <p className="text-figma_grey text-label uppercase w-fit">
+          participants
+        </p>
+      ) : null}
+      <div className="w-full flex flex-col gap-8 px-2 py-4 bg-figma_shade2_30 shadow rounded-sm ">
+        <ParticipantsList />
 
-export default ParticipantsPanel;
+        {status === "Editing" ? (
+          <div className="w-full flex justify-between">
+            <EditorButton
+              text={<Shuffle size={"24px"} />}
+              onClickHandler={shuffleParticipants}
+            />
+            <EditorButton
+              text="Add Participant"
+              onClickHandler={addParticipant}
+            />
+          </div>
+        ) : null}
+      </div>
+      {status !== "Editing" ? (
+        <p className="text-center text-grey text-desc">
+          Note: You cannot edit the participants after the bracket has started.
+          Reset the bracket to make changes.
+        </p>
+      ) : null}
+    </div>
+  )
+}
+
+export default ParticipantsPanel
--- frontend/src/features/bracket/bracket-panel/components/play-panel/play-panel.tsx
+++ frontend/src/features/bracket/bracket-panel/components/play-panel/play-panel.tsx
@@ -1,36 +1,45 @@
 // import EditorButton from "../../../components/editor-button";
-import PlayProgress from "./play-progress";
-import SaveChangeButton from "../save-changes-button";
+import PlayProgress from "./play-progress"
+import SaveChangeButton from "../save-changes-button"
 
-const PlayPanel = () => {
-	return (
-		<div className="w-full h-full flex flex-col gap-2.5 p-4">
-			{/* Bracket Status */}
-			<p className="text-figma_grey text-label uppercase w-fit">
-				bracket status
-			</p>
-			<div className="w-full flex flex-col gap-8 px-2 py-4 bg-figma_shade2_30 shadow rounded-sm">
-				<PlayProgress />
-			</div>
+interface PlayPanelProps {
+  isTitleVisible?: boolean
+}
 
-			{/* Changes */}
-			{/* TODO: Hook up this feature to save on a database */}
-			<div className="w-full flex flex-col gap-8 px-2 py-4 bg-figma_shade2_30 shadow rounded-sm">
-				<div className="w-full pb-2 border-b border-figma_neutral8 ">
-					<p className="text-desc text-center text-figma_grey">
-						Changes Unsaved
-					</p>
-				</div>
-				<p className="text-desc text-center text-figma_grey">
-					Save your changes to make sure your work doesn&apos;t disappear!
-				</p>
-				<div className="flex justify-center items-center">
-					<SaveChangeButton />
-				</div>
-			</div>
+const PlayPanel = ({ isTitleVisible = true }: PlayPanelProps) => {
+  return (
+    <div className="w-full h-full flex flex-col gap-2.5 py-4 md:px-4">
+      {/* Bracket Status */}
+      {isTitleVisible ? (
+        <p
+          className="text-figma_grey text-label uppercase w-fit 
+        "
+        >
+          bracket status
+        </p>
+      ) : null}
+      <div className="w-full flex flex-col gap-8 px-2 py-4 bg-figma_shade2_30 shadow rounded-sm">
+        <PlayProgress />
+      </div>
 
-			{/* Bracket Status */}
-			{/* <div className="w-full flex flex-col gap-4 px-2 py-4 bg-figma_shade2_30 shadow rounded-sm">
+      {/* Changes */}
+      {/* TODO: Hook up this feature to save on a database */}
+      <div className="w-full flex flex-col gap-8 px-2 py-4 bg-figma_shade2_30 shadow rounded-sm">
+        <div className="w-full pb-2 border-b border-figma_neutral8 ">
+          <p className="text-desc text-center text-figma_grey">
+            Changes Unsaved
+          </p>
+        </div>
+        <p className="text-desc text-center text-figma_grey">
+          Save your changes to make sure your work doesn&apos;t disappear!
+        </p>
+        <div className="flex justify-center items-center">
+          <SaveChangeButton />
+        </div>
+      </div>
+
+      {/* Bracket Status */}
+      {/* <div className="w-full flex flex-col gap-4 px-2 py-4 bg-figma_shade2_30 shadow rounded-sm">
 				<p className="text-desc text-center text-figma_grey">
 					Let others know about your bracket!
 				</p>
@@ -38,8 +47,8 @@ const PlayPanel = () => {
 					<EditorButton text="share" />
 				</div>
 			</div> */}
-		</div>
-	);
-};
+    </div>
+  )
+}
 
-export default PlayPanel;
+export default PlayPanel
